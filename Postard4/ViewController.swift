04f6651e56146546d6ad670e5f1d76a60b746e92
Agi1233 <agnieszka.kopec1@wp.pl>
2014-11-04 21:22:22 +0100

    first rev of project

---
+++ Postard4/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Postard4
+//
+//  Created by Agnieszka Purc on 02.11.2014.
+//  Copyright (c) 2014 Agnieszka Purc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mesaggeLebel: UILabel!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMesaggeTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendmailButtonPressed(sender: UIButton) {
+        mesaggeLebel.hidden = false
+        mesaggeLebel.text = enterMesaggeTextField.text
+        mesaggeLebel.textColor = UIColor.redColor()
+        
+        enterMesaggeTextField.text = ""
+        enterMesaggeTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
+
+}
+
